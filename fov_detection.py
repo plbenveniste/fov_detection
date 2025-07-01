50ede674604647f15146de7fde784663e238e186
n-bnv <nathanbenveniste@gmail.com>
2025-07-01 11:03:26 +0200

    Base program that identifies vertebrae
    Determining wether the image contains Cervical, Thoracic or Lumbar vertebrae.

---
+++ fov_detection.py
@@ -0,0 +1,24 @@
+import nibabel as nib # type: ignore
+file_path = "/Users/nathanbenveniste/Desktop/Coding Projects/Python Projects/AI_Internship/seg_img_step2_output.nii.gz"
+img = nib.load(file_path)
+data = img.get_fdata()
+print(f"Dimensions : {data.shape}")
+# ici on print les valeurs uniques
+unique_values = set(data.flatten())
+print(f"Unique values in the image: {unique_values}")
+# on détermine quels types de vertèbres sont présents
+C,T,L = False,False,False
+# Cervical vertebrae: 11-17
+# Thoracic vertebrae: 21-32
+# Lumbar vertebrae: 41-45
+# On vérifie si les valeurs uniques contiennent des vertèbres cervicales, thoraciques ou lombaires
+for x in unique_values:
+    if 11<= x <= 17:
+        C = True
+    if 21<= x <= 32:
+        T = True
+    if 41<= x <= 45:
+        L = True
+print(f"Cervical vertebrae present: {C}")
+print(f"Thoracic vertebrae present: {T}")
+print(f"Lumbar vertebrae present: {L}")
