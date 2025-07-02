7a68e8fcce736f2b87da2c5e94c4935782119334
Nathan Benveniste <nathanbenveniste@MacBook-Air-de-Nathan.local>
2025-07-02 16:56:33 +0200

    updated version to run on all images

--- img_chunk_detection/run_all_imgs.py
+++ img_chunk_detection/run_all_imgs.py
@@ -3,12 +3,11 @@ from run_totalspineseg import totalspineseg
 from fov_detection import from_output_to_bool, from_output_to_csv
 import pathlib as Path
 import argparse
-
+import os
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Identify vertebrae in a NIfTI image")
     parser.add_argument('-i', type=str, required=True, help='Path to the folder containing NIfTI images serving as input for totalspineseg')
-    parser.add_argument('-o', type=str, required=True, help='Path to save the segmented image returned by totalspineseg')
     parser.add_argument('-path_to_csv', type=str, required=True, help='Path to save the CSV file containing the vertebrae information for each image')
     return parser.parse_args()
 
@@ -25,26 +24,27 @@ def get_all_imgs(input_path):
 
 
 def run_all_imgs(input_path, output_path, path_to_csv):
-    """
-    Run totalspineseg on the input image and then process the output to generate a CSV file.
-    """
     # Run totalspineseg
     totalspineseg(input_path, output_path)
-
     # Process the output to get vertebrae information
     output_path = output_path.split(".nii.gz")
     output_path = "".join(output_path) + "_step2_output.nii.gz"
     C, T, L = from_output_to_bool(output_path)
     # Save the vertebrae information to a CSV file
-    from_output_to_csv(output_path, path_to_csv, C, T, L)
+    from_output_to_csv(input_path, path_to_csv, C, T, L)
 
 
 if __name__ == "__main__":
     args = parse_args()
     input_path = args.i
-    output_path = args.o
+    assert os.system("mkdir ~/Desktop/temp")==0
+    output_path = "~/Desktop/temp/seg_img.nii.gz"
     path_to_csv = args.path_to_csv
-    # imgs = get_all_imgs(input_path)
-    run_all_imgs(input_path, output_path, path_to_csv)
-    # print(imgs[0:10])
-    # print(len(imgs))  # Print the total number of images found
+    imgs = get_all_imgs(input_path)
+    print(imgs)
+    # Loop through all images and process them
+    for img in imgs:
+        print(f"Processing image: {img}")
+        # Process the output to generate CSV
+        run_all_imgs(img, output_path, path_to_csv)
+    assert os.system("rm -r ~/Desktop/temp")==0
